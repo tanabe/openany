06542096b4966473d633a21119098ba74b03bdd7
Hideaki Tanabe <tanablog@gmail.com>
2022-03-28 23:36:46 +0900

    run open command

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,20 @@
-console.log('hello world');
+import { exec } from 'child_process';
+const args = process.argv.slice(2);
+
+if (args.length != 1) {
+    console.log('usage: anyopen <source>');
+    process.exit(1);
+}
+
+let source = args[0];
+let command = `open ${source}`;
+exec(command, (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+});
